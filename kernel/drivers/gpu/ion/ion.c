c30cadfc7bf789cd438989116507bf90b34c9128
Mayeul Cantan <mayeul.cantan@gmail.com>
2018-01-30 16:12:02 +0100

    Open source archive for build 18.1.A.1.17
    Sony Xperia T3 copyleft release from:
    https://developer.sonymobile.com/downloads/xperia-open-source-archives/open-source-archive-for-build-18-1-a-1-17/
    
    Direct link:
    https://dl.developer.sonymobile.com/code/copylefts/18.1.A.1.17.tar.bz2
    
    Official description:
    Open source download for Xperia T3; software version 18.1.A.1.17.
    
    Release date:
    August 13, 2014

--- kernel/drivers/gpu/ion/ion.c
+++ kernel/drivers/gpu/ion/ion.c
@@ -653,6 +653,7 @@ static void ion_handle_kmap_put(struct ion_handle *handle)
 {
 	struct ion_buffer *buffer = handle->buffer;
 
+	BUG_ON(!handle->kmap_cnt);
 	handle->kmap_cnt--;
 	if (!handle->kmap_cnt)
 		ion_buffer_kmap_put(buffer);
